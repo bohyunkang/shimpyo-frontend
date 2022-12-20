96f2fcaa06abafaff6ee4c6f90ba1b3e2e0514a9
Bohyun Kang <bbohyun@kakao.com>
2022-12-20 13:14:09 +0900

    쉼표 프론트엔드 프로젝트 생성
    - 필요한 라이브러리 설치 및 프로젝트 초기화

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+
+root.render((
+  <App />
+));
