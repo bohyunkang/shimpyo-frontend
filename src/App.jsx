624299e5bba236f5b4c716bcad07a6852042f132
Bohyun Kang <bbohyun@kakao.com>
2023-02-08 15:47:30 +0900

    비로그인 시 접근 불가능하게 라우팅 처리
    - PrivateRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { useLocalStorage } from 'usehooks-ts';
 import { ThemeProvider } from 'styled-components';
 import { Reset } from 'styled-reset';
+
+import useUserStore from './hooks/useUserStore';
 
 import theme from './styles/theme';
 import mixin from './styles/mixin';
 import GlobalStyle from './styles/GlobalStyle';
+
+import ProtectedRoute from './ProtectedRoute';
 
 import HomePage from './pages/Home/HomePage';
 import CategoryPage from './pages/Category/CategoryPage';
@@ -12,9 +18,25 @@ import ReservationPage from './pages/reservation/ReservationPage';
 import ReservationHistoryPage from './pages/reservation/ReservationHistoryPage';
 import ProfilePage from './pages/profile/ProfilePage';
 import ProgramDetailPage from './pages/program/ProgramDetailPage';
+import LoginPage from './pages/login/LoginPage';
+import SignupPage from './pages/signup/SignupPage';
 import ScrollToTop from './components/common/ScrollToTop';
+import { apiService } from './services/ApiService';
 
 export default function App() {
+  const [accessToken] = useLocalStorage('accessToken', '');
+
+  const userStore = useUserStore();
+
+  useEffect(() => {
+    // TODO: 프로세스 확인 필요!
+    apiService.setAccessToken(accessToken);
+
+    if (accessToken) {
+      userStore.fetchUser();
+    }
+  }, [accessToken]);
+
   return (
     <ThemeProvider theme={{ ...theme, ...mixin }}>
       <Reset />
@@ -22,11 +44,34 @@ export default function App() {
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/programs" element={<CategoryPage />} />
         <Route path="/programs/:id" element={<ProgramDetailPage />} />
-        <Route path="/reservation" element={<ReservationPage />} />
-        <Route path="/reservations" element={<ReservationHistoryPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route
+          path="/reservation"
+          element={(
+            <ProtectedRoute accessToken={accessToken}>
+              <ReservationPage />
+            </ProtectedRoute>
+          )}
+        />
+        <Route
+          path="/reservations"
+          element={(
+            <ProtectedRoute accessToken={accessToken}>
+              <ReservationHistoryPage />
+            </ProtectedRoute>
+          )}
+        />
+        <Route
+          path="/profile"
+          element={(
+            <ProtectedRoute accessToken={accessToken}>
+              <ProfilePage />
+            </ProtectedRoute>
+          )}
+        />
       </Routes>
     </ThemeProvider>
   );
